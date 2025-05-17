a876886d0fa0543cb8c8ed8235a18fc89a00c449
Mathew <mateusz.lu123@gmail.com>
2025-05-17 19:40:53 +0200

    added leftovers functions to hook that create stuff

--- src/customhooks/useEditorCommands.ts
+++ src/customhooks/useEditorCommands.ts
@@ -1,12 +1,23 @@
-
 import { EditorState, EditorUpdate } from "../types/editor";
-import { changeTag, createElementInRange, deleteNode, getNodesInRange, getParentNodeWithTag, getRangeBetweenElements, getStatusAboutCurrentColumns, SelectionRestorerFromText } from "../Utilities/editors";
+import {
+  changeTag,
+  createElementInRange,
+  deleteNode,
+  getNodesInRange,
+  getParentNodeWithTag,
+  getRangeBetweenElements,
+  getStatusAboutCurrentColumns,
+  groupElementsByParent,
+  SelectionRestorerFromText,
+  splitNodes,
+  splitNodesInRange,
+} from "../Utilities/editors";
 import { HistoryOfText } from "./useHistorySaver";
 
 export function useEditorCommands(
   state: EditorState,
-  update: EditorUpdate,
-  textDocument: HistoryOfText
+  textDocument: HistoryOfText,
+  savedSelection: Range | null
 ) {
   const addList = (listType: string) => {
     const selection = window.getSelection();
@@ -70,7 +81,11 @@ export function useEditorCommands(
 
       range.setStart(parentNodeOfStart, startOffset);
       range.setEnd(parentNodeOfEnd, endOffset);
-      textDocument.saveValue(state.markdownInput.current!.innerHTML, true, true);
+      textDocument.saveValue(
+        state.markdownInput.current!.innerHTML,
+        true,
+        true
+      );
       return;
     }
 
@@ -107,7 +122,11 @@ export function useEditorCommands(
         deleteNode(parentList);
         range.setStart(parentNodeOfStart, startOffset);
         range.setEnd(parentNodeOfEnd, endOffset);
-        textDocument.saveValue(state.markdownInput.current!.innerHTML, true, true);
+        textDocument.saveValue(
+          state.markdownInput.current!.innerHTML,
+          true,
+          true
+        );
         return;
       }
 
@@ -128,7 +147,11 @@ export function useEditorCommands(
 
       range.setStart(parentNodeOfStart, startOffset);
       range.setEnd(parentNodeOfEnd, endOffset);
-      textDocument.saveValue(state.markdownInput.current!.innerHTML, true, true);
+      textDocument.saveValue(
+        state.markdownInput.current!.innerHTML,
+        true,
+        true
+      );
       return;
     }
 
@@ -177,8 +200,215 @@ export function useEditorCommands(
     textDocument.saveValue(state.markdownInput.current!.innerHTML, true, true);
   };
 
-  return {addList}
+  const addImage = ({
+    imageUrl,
+    passedRange,
+  }: {
+    imageUrl: string;
+    passedRange?: Range | null;
+  }) => {
+    let selection = window.getSelection();
+    if (selection === null) return;
+
+    if (state.markdownInput.current === null) return;
+
+    let range = passedRange ? passedRange : selection.getRangeAt(0);
+    const parentNodeOfStart = getParentNodeWithTag(range.startContainer, [
+      "p",
+      "li",
+    ])! as HTMLElement;
+
+    const startRange = new Range();
+    startRange.setStart(range.startContainer, range.startOffset);
+    startRange.setEnd(range.startContainer, range.startOffset);
+    let splitNode = splitNodes(parentNodeOfStart, startRange);
+    if (splitNode) {
+      range.setStartBefore(splitNode);
+      range.setEndBefore(splitNode);
+    }
+
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    image.style.maxWidth = "100%";
+
+    range.insertNode(image);
+    textDocument.saveValue(state.markdownInput.current!.innerHTML, true, true);
+  };
+
+  const addLink = ({
+    linkName,
+    linkText,
+    passedRange = null,
+  }: {
+    linkName: string;
+    linkText?: string | undefined;
+    passedRange?: Range | null;
+  }) => {
+    let selection = window.getSelection();
+    if (selection === null) return;
+
+    if (state.markdownInput.current === null) return;
+
+    let range = passedRange ? passedRange : selection.getRangeAt(0);
+    const parentNode = getParentNodeWithTag(range.startContainer, ["p", "li"]);
+
+    if (!parentNode) return;
+    if (range.toString() === "") {
+      const nodeSplit = splitNodes(parentNode, range);
+      if (nodeSplit) {
+        range.setStartBefore(nodeSplit);
+        range.setEndBefore(nodeSplit);
+      }
+      const link = createElementInRange("a", range);
+      link.setAttribute("href", linkName);
+      link.textContent = linkText ? linkText : linkName;
+
+      range.setStart(link, 0);
+      range.setEnd(link, link.childNodes.length);
+      createElementInRange("span", range);
+
+      if (nodeSplit?.textContent === "") deleteNode(nodeSplit);
+    } else {
+      const allSpansAndText = getNodesInRange(
+        range,
+        ["SPAN", "#text"],
+        state.markdownInput.current!
+      );
+      const allSpans = allSpansAndText.filter((node) => {
+        const element = node as HTMLElement;
+        return element.nodeName === "SPAN" && element.classList.length === 0;
+      });
+      const allTextWithoutSpan = allSpansAndText.filter((element) => {
+        return (
+          element.nodeName === "#text" &&
+          element.parentNode?.nodeName !== "SPAN"
+        );
+      });
+      allTextWithoutSpan.forEach((text) => {
+        const spanElement = document.createElement("span");
+        const clonedNode = text.cloneNode(true) as HTMLElement;
+
+        spanElement.appendChild(clonedNode);
+        text.parentNode?.replaceChild(spanElement, text);
+        allSpans.push(spanElement);
+      });
+      const groupedElements = groupElementsByParent(allSpans as HTMLElement[]);
+
+      groupedElements.forEach((group, parent) => {
+        const rangeWithin = new Range();
+        const firstElement = group[0];
+        let lastElement = group[group.length - 1];
+
+        const startContainer = range.startContainer;
+        const startOffset = range.startOffset;
+        const isGroupOneElement = firstElement === lastElement;
+
+        const rangeToRestore = new SelectionRestorerFromText();
+        rangeToRestore.saveRange(
+          range,
+          range.startContainer,
+          range.endContainer
+        );
+
+        const nodeRange = document.createRange();
+
+        nodeRange.selectNodeContents(lastElement);
+        if (nodeRange.compareBoundaryPoints(Range.END_TO_END, range) > 0) {
+          const endRange = new Range();
+          endRange.setStart(range.endContainer, range.endOffset);
+          endRange.setEnd(range.endContainer, range.endOffset);
+          const splitNode = splitNodesInRange(lastElement, endRange);
+
+          rangeWithin.setEndBefore(splitNode.nodeAfterSplit);
+        } else {
+          rangeWithin.setEndAfter(lastElement);
+        }
+
+        nodeRange.selectNodeContents(firstElement);
+        if (nodeRange.compareBoundaryPoints(Range.START_TO_START, range) < 0) {
+          if (isGroupOneElement) {
+            range.setStart(startContainer, startOffset);
+            range.setEnd(startContainer, startOffset);
+          }
+          const splitNode = splitNodesInRange(firstElement, range);
+
+          rangeWithin.setStartBefore(splitNode.nodeAfterSplit);
+          if (isGroupOneElement) {
+            rangeWithin.setEndAfter(splitNode.nodeAfterSplit);
+          }
+        } else {
+          rangeWithin.setStartBefore(firstElement);
+        }
+
+        const linkElement = document.createElement("a");
+        linkElement.href = linkName;
+
+        rangeWithin.surroundContents(linkElement);
+      });
+    }
+    textDocument.saveValue(state.markdownInput.current!.innerHTML, true, true);
+  };
+
+  const createTable = (rows: number, columns: number) => {
+    let range = savedSelection;
+    range?.setEnd(range.startContainer, range.startOffset);
+
+    if (!range || !state.markdownInput.current?.contains(range.startContainer))
+      return;
+
+    const parent = getParentNodeWithTag(range.startContainer, [
+      "P",
+      "UL",
+      "OL",
+    ]);
+    let marginLeft = "0pt";
+    let marginRight = "0pt";
+    if (parent) {
+      const split = splitNodesInRange(parent, range);
+      const computedStyle = getComputedStyle(parent as HTMLElement);
+      marginLeft = computedStyle.marginLeft;
+      marginRight = computedStyle.marginRight;
+
+      range.setStartBefore(split.nodeAfterSplit);
+      range.setEndBefore(split.nodeAfterSplit);
+    }
+
+    // Create a new table element
+    const container = range.startContainer as HTMLElement;
+    const containerWidth = container.offsetWidth;
+
+    const table = document.createElement("table");
+    const tableWidth =
+      containerWidth - parseFloat(marginLeft) - parseFloat(marginRight);
+    table.style.marginLeft = marginLeft;
+    table.style.marginRight = marginRight;
+    table.style.border = "1px solid;";
+    table.style.borderCollapse = "collapse";
+    table.style.display = "block"; // to prevent selection next to table
+    const rowWidth = tableWidth / rows;
+    // Create table rows
+    for (let i = 0; i < rows; i++) {
+      const row = table.insertRow();
+      row.style.minHeight = "1em";
+      row.style.width = `${rowWidth}px`;
+      // Create table cells in each row
+      for (let j = 0; j < columns; j++) {
+        const cell = row.insertCell();
+        cell.style.width = `${rowWidth}px`;
+        cell.style.border = "1px solid black";
+        cell.style.wordBreak = "break-all";
+        cell.style.minHeight = "1em";
+        cell.style.overflow = "hidden";
+        const span = document.createElement("span");
+        span.innerHTML = "&#x200b;";
+        cell.appendChild(span);
+      }
+    }
+    // Insert the table into the document
+    range?.insertNode(table);
+
+    textDocument.saveValue(state.markdownInput.current!.innerHTML, true, false);
+  };
+
+  return { addList, addImage, addLink, createTable };
 }
-
-
-
